7443502a26bc1db6f09929541a1b4f5a755a126c
aditya-kokashe <adi310kokashe@gmail.com>
2026-05-17 16:52:57 +0530

    [Aditya Kokashe] : updated frontend

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -186,7 +186,10 @@ export default function Register() {
             <button
               type="button"
               className="btn btn-google btn-full"
-              onClick={() => window.location.href = 'http://localhost:8080/oauth2/authorization/google'}
+              onClick={() => {
+                const apiBase = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080';
+                window.location.href = `${apiBase}/oauth2/authorization/google`;
+              }}
             >
               <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4"/>
